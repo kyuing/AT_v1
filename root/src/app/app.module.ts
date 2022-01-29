ea814834758fbae0f7e14769dc06e90871bf0e55
kyuing <kyykyu000@gmail.com>
2022-01-29 19:54:50 +0000

    v1.1: .... directives

--- root/src/app/app.module.ts
+++ root/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseComponent,
     CoursesComponent
   ],
   imports: [
--- root/src/app/courses.component.ts
+++ root/src/app/courses.component.ts
@@ -2,10 +2,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '' //<courses>
-  template: '<h2>Courses</h2>'
+  // selector: 'courses',
+  // template: '<h2>{{ "Title: " + title }}</h2>'
+  // template: '<h2>{{ getTitle() }}</h2>' //call getter
 
+  selector: 'courses',
+  //directive
+  template: `
+    <h2>{{ "Title: " + title }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `
 })
 export class CoursesComponent {
-
+  title = "List of courses";
+  courses = ["courses1", "courses2", "courses3"];
+  // getTitle() {
+  //   return this.title;
+  // }
 }
